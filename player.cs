204d243044ae2d2376c86c11ce46064f2e401e60
Igor Sena <gunsigoroses@gmail.com>
2020-05-16 22:58:54 -0300

    Add files via upload

---
+++ player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+	public float Speed;
+	public float JumpForce;
+	
+	private Rigidbody2D rig;
+	private bool isJumping;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+			rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		rig.velocity = new Vector2(Speed, rig.velocity.y);
+		if (Input.GetMouseButtonDown(0) && !isJumping){
+		rig.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);	
+		isJumping = true;
+		}
+    }
+	
+	// Update is called once per frame
+    void OnCollisionEnter2D(Collision2D colisor){
+		if (colisor.gameObject.layer ==8){
+		isJumping = false;
+		}
+    }
+}
